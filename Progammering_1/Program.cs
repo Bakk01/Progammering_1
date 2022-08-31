17ebc0ca1c368d7826a5cea83ba7821c90f14d70
Batkhuslen Boldbaatar <Batkhuslen.Boldbaatar@elev.ga.ntig.se>
2022-08-31 14:23:05 +0200

    initial commit

---
+++ Progammering_1/Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+//Console.WriteLine("Hej Nti");
+//Console.WriteLine("DET ÄR JAG SOM ÄR BULTEN.");
+
+//string name;
+
+//name = "Bullen";
+//name = "Bulten";
+
+//Console.WriteLine("vad heter du?");
+//name = Console.Readline(name);
+
+//Console.WriteLine($"Välkommen {}  !");
+
+
+Console.ReadLine();
+
+Console.WriteLine("Hej Vad heter du?");
+
+string name;
+
+name = Console.ReadLine();
+
+Console.WriteLine("Vad gillar du för mat?");
+
+string mat;
+
+mat = Console.ReadLine();
+
+Console.WriteLine("Hur lång är du?");
+
+string längd;
+
+längd = Console.ReadLine();
+
+Console.WriteLine($"Hej {name} Jag har hört att du gillar mycket {mat} men blir du inte trött att äte så mycket {mat}? Men det är ändå bra eftersom att du behöver ändå växa eftersom att du är {längd} :=)");
+
+name = Console.ReadLine();
+mat = Console.ReadLine();
+längd = Console.ReadLine();
+
+
